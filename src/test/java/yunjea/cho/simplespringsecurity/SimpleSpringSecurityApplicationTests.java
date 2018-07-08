5f7a83f0db4063a954ee4be318ae501dc5c0412a
comp1 <chotom73@gmail.com>
2018-07-08 10:52:55 +0900

    Initial commit

---
+++ src/test/java/yunjea/cho/simplespringsecurity/SimpleSpringSecurityApplicationTests.java
@@ -0,0 +1,16 @@
+package yunjea.cho.simplespringsecurity;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SimpleSpringSecurityApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
